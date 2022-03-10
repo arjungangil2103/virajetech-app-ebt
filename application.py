6db328b5db69b0835d8485b984145ab72c625267
arjungangil2103 <47173762+arjungangil2103@users.noreply.github.com>
2022-03-10 11:01:34 +0530

    adad
    ad

--- application.py
+++ application.py
@@ -108,7 +108,7 @@ welcome = """
   <div class="textColumn">
     <h1>Congratulations</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
-    <p>This environment is launched with Elastic Beanstalk Python Platform - www.virajetech.com - virajetech - nnnthanks  ,,,</p>
+    <p>This environment is launched with Elastic Beanstalk Python Platform - www.virajetech.com - virajetech - nnnthanks hdfh ,,,</p>
   </div>
   
   <div class="linksColumn"> 
